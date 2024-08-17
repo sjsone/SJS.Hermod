bcf9cf64e56f23f3b49f038501050b67ea3e7ea8
sjsone <simon@sjs.one>
2024-08-17 13:32:57 +0200

    chore: created `LokiCommandController`

---
+++ Classes/Command/LokiCommandController.php
@@ -0,0 +1,79 @@
+<?php
+namespace SJS\Hermod\Command;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Cli\CommandController;
+use SJS\Hermod\Exception;
+use SJS\Hermod\Client\LokiClient;
+use SJS\Hermod\Client\LokiClient\Configuration;
+use Psr\Log\LoggerInterface;
+use Neos\Flow\Configuration\ConfigurationManager;
+use Neos\Flow\Log\Utility\LogEnvironment;
+
+class LokiCommandController extends CommandController
+{
+    #[Flow\Inject]
+    protected LoggerInterface $systemLogger;
+    
+    #[Flow\Inject]
+    protected ConfigurationManager $configurationManager;
+
+    protected function createClientWithoutFallbackFile(array $configuration): LokiClient
+    {
+        $lokiClientConfiguration = new Configuration(
+            $configuration['user'],
+            $configuration['token'],
+            $configuration['url'],
+            $configuration['connectTimeout'],
+            $configuration['readTimeout'],
+            $configuration['labels'],
+        );
+
+        return new LokiClient($lokiClientConfiguration);
+    }
+
+    public function sendFallbackCommand(string $configurationPath = "")
+    {
+        $configuration = $this->configurationManager->getConfiguration("Settings", $configurationPath);
+        if(!$configuration || !is_array($configuration)) {
+            \Neos\Flow\var_dump($configuration);
+            die();
+            $this->output("<error>Could not read configuration</error>\n");
+            return;
+        }
+
+        $client = $this->createClientWithoutFallbackFile($configuration);
+
+        $fallbackFile = $configuration["fallbackFile"] ?? null;
+        if(!is_string($fallbackFile)) {
+            $this->output("<error>Incompatible type: </error> $fallbackFile\n");
+            return;
+        }
+
+        $fallbackFileHandler = \fopen($fallbackFile, "r+");
+        if(!$fallbackFileHandler) {
+            $this->output("<error>Could not open file handler for: </error> $fallbackFile\n");
+            return;
+        }
+
+        while (($line = fgets($fallbackFileHandler)) !== false) {
+            try {
+                $stream = json_decode($line);
+                $client->send([$stream]);
+            }catch(\Throwable $th) {
+                // TODO: log error in json encoding
+            }
+
+            $pos = ftell($fallbackFileHandler);
+            $remaining = stream_get_contents($fallbackFileHandler);
+            fseek($fallbackFileHandler, 0);    
+            fwrite($fallbackFileHandler, $remaining);
+    
+            ftruncate($fallbackFileHandler, ftell($fallbackFileHandler));
+    
+            fseek($fallbackFileHandler, $pos - strlen($line));
+        }
+    
+        fclose($fallbackFileHandler);
+    }
+}
